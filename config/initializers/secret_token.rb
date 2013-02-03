539a204c238f686cf0d269c2cf5da2f237c2f0f8
John Kealy <jdkealy@gmail.com>
2013-02-02 23:18:31 -0500

    cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoads::Application.config.secret_token = '996c682f41e2e265c09f60389715299f63820ebb80c54fc6e3595f900ee8df1c2ced8c1fa6a517c87b912732581db8b62e75d72bc5ebc74ea800df966cadc57b'
