95191cfc307c50e9d5c6c460423e08011e3b24e8
John Kealy <jdkealy@gmail.com>
2013-02-03 14:03:56 -0500

    nicer cucumber step definitions

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -7,15 +7,13 @@ When /^I go to the homepage$/ do
   visit '/'
 end
 
-Then /^I should see a sign out link$/ do
-  find_link('Sign Out').visible?.should == true
+Then /^I should see a link (.+)$/ do |link|
+  puts link
+  n_link = find_link(link) rescue nil
+  n_link.should_not == nil
 end
 
-Then /^I should see a sign in link$/ do
-  find_link('Sign In').visible?.should == true
-end
-
-Then /^I should not see a sign out link$/ do
-  link = find_link('Sign Out') rescue nil
-  link.should == nil
+Then /^I should not see a link (.+)$/ do |link|
+  n_link = find_link(link) rescue nil
+  n_link.should == nil
 end
--- features/user.feature
+++ features/user.feature
@@ -2,10 +2,10 @@ Feature: Discover
   Scenario: User sees sign out
     Given a user
     When I go to the homepage
-    Then I should see a sign out link
-  Scenario: Regular user does not see sign out
+    Then I should see a link Sign Out
+  Scenario: Regular user does not see Sign Out
     When I go to the homepage
-    Then I should not see a sign out link
+    Then I should not see a link sign out
   Scenario: Regular user sees sign in
     When I go to the homepage
-    Then I should see a sign in link
+    Then I should see a link Sign In
