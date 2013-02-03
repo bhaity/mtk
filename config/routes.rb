eb97895b90390028d3cc650a2171dce060e5238d
John Kealy <jdkealy@gmail.com>
2013-02-02 23:54:46 -0500

    use controller instead of static html file

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Photoads::Application.routes.draw do
   # Sample resource route with more complex sub-resources
   #   resources :products do
   #     resources :comments
-  #     resources :sales do
+  #     resources :oales do
   #       get 'recent', :on => :collection
   #     end
   #   end
@@ -50,7 +50,7 @@ Photoads::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
